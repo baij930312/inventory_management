b1c38fd35f88f54966fbfbae66591928e791f1e7
jinWarr <baij930312@163.com>
2019-08-17 13:40:32 +0800

    change package name for android

---
+++ android/app/src/main/java/com/app/inventorymanagement/App.java
@@ -0,0 +1,27 @@
+package com.app.inventorymanagement;
+
+import android.content.Context;
+
+import io.flutter.app.FlutterApplication;
+
+/**
+ * Created by Administrator
+ *
+ * @author 猿史森林
+ *         Date: 2017/11/28
+ *         Class description:
+ */
+public class App extends FlutterApplication {
+
+    private static Context mContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mContext = getApplicationContext();
+    }
+
+    public static Context getContext() {
+        return mContext;
+    }
+}
